a2b29bae23017c24ab6eb568fca09745abac834a
Johannes <johannesamadeusm@gmail.com>
2024-04-06 22:06:04 +0800

    updated key names

--- main.py
+++ main.py
@@ -4,41 +4,35 @@ from Cryptodome.Signature import PKCS1_v1_5
 from Cryptodome.Cipher import PKCS1_OAEP
 import os
 
-SENDER_KEYS_FOLDER_NAME = "sender_keys"
-RECEIVER_KEYS_FOLDER_NAME = "receiver_keys"
-
 def genMessage(message):
-    with open("message.in", "w") as f:
+    with open("message.txt", "w") as f:
         f.write(message)
         
 # Every time this function is called, new keypairs are generated
-def genKeypair():
-    # Create sender and receiver keys folder if it doesn't exist
-    if not os.path.exists(SENDER_KEYS_FOLDER_NAME):
-        os.makedirs(SENDER_KEYS_FOLDER_NAME)
-    if not os.path.exists(RECEIVER_KEYS_FOLDER_NAME):
-        os.makedirs(RECEIVER_KEYS_FOLDER_NAME)
-
-    # Generating sender's keys
-    sender_key = RSA.generate(2048)
-    sender_private_key = sender_key.export_key()
-    with open(os.path.join(SENDER_KEYS_FOLDER_NAME, "private.pem"), "wb") as f:
-        f.write(sender_private_key)
-    sender_public_key = sender_key.publickey().export_key()
-    with open(os.path.join(SENDER_KEYS_FOLDER_NAME, "sender.pem"), "wb") as f:
-        f.write(sender_public_key)
+def genKeypairs():
+    if not os.path.exists("keys"):
+        os.makedirs("keys")
+    
+    # Generating encryption key-pair
+    encryption_key = RSA.generate(2048)
+    private = encryption_key.export_key()
+    with open(os.path.join("keys","enc_private.pem"), "wb") as f:
+        f.write(private)
+    public = encryption_key.publickey().export_key()
+    with open(os.path.join("keys","enc_public.pem"), "wb") as f:
+        f.write(public)
         
-    # Generating receiver's keys
-    receiver_key = RSA.generate(2048)
-    receiver_private_key = receiver_key.export_key()
-    with open(os.path.join(RECEIVER_KEYS_FOLDER_NAME, "private.pem"), "wb") as f:
-        f.write(receiver_private_key)
-    receiver_public_key = receiver_key.publickey().export_key()
-    with open(os.path.join(RECEIVER_KEYS_FOLDER_NAME, "receiver.pem"), "wb") as f:
-        f.write(receiver_public_key)
-
+    # Generating signing key-pair
+    signing_key = RSA.generate(2048)
+    signing = signing_key.export_key()
+    with open(os.path.join("keys","signing.pem"), "wb") as f:
+        f.write(signing)
+    verifying = signing_key.publickey().export_key()
+    with open(os.path.join("keys","verifying.pem"), "wb") as f:
+        f.write(verifying)
+        
 def encrypt(plaintext, public_key):
-    with open("message.in", "r") as f:
+    with open("message.txt", "r") as f:
         plaintext = f.read()
     plaintext = plaintext.encode("ascii")
     cipher_rsa = PKCS1_OAEP.new(public_key)
@@ -70,41 +64,34 @@ def verifySignature(ciphertext, signature, public_key):
 def main():
     while True:
         print("1. Generate new keypairs")
-        print("2. Encrypt message")
-        print("3. Decrypt message")
-        print("4. Add signature")
-        print("5. Verify signature")
-        print("6. Exit")
+        print("2. Encrypt and Sign Message")
+        print("3. Verify signature and Decrypt Message")
+        print("4. Exit")
         choice = input("Select Operation: ")
         
         if choice == "1":
             print("Generating new keypairs...")
-            genKeypair()
+            genKeypairs()
             os.system("cls")
             print("\nKeypairs generated successfully.\n")
             
         elif choice == "2":
             message = input("Enter the message: ")
             genMessage(message)
-            t = encrypt(message, RSA.import_key(open(os.path.join(RECEIVER_KEYS_FOLDER_NAME, "receiver.pem")).read()))
+            t = encrypt(message, RSA.import_key(open(os.path.join("keys", "enc_public.pem")).read()))
+            s = addSignature(t, RSA.import_key(open(os.path.join("keys", "signing.pem")).read()))
             print("\nSuccessfully encrypted message.\n")
+            print(s)
             
         elif choice == "3":
-            d = decrypt(t, RSA.import_key(open(os.path.join(RECEIVER_KEYS_FOLDER_NAME, "private.pem")).read()))
-            print(d.decode("ascii") + "\n")
-            
-        elif choice == "4":
-            s = addSignature(t, RSA.import_key(open(os.path.join(SENDER_KEYS_FOLDER_NAME, "private.pem")).read()))
-            print(s)
-            print()
-            
-        elif choice == "5":
-            if verifySignature(t, s, RSA.import_key(open(os.path.join(SENDER_KEYS_FOLDER_NAME, "sender.pem")).read())):
+            if verifySignature(t, s, RSA.import_key(open(os.path.join("keys", "verifying.pem")).read())):
                 print("\nSignature verification successful.\n")
+                d = decrypt(t, RSA.import_key(open(os.path.join("keys", "enc_private.pem")).read()))
+                print(d.decode("ascii") + "\n")
             else:
                 print("\nSignature verification failed.\n")
                
-        elif choice == "6":
+        elif choice == "4":
             break
             
         else:
